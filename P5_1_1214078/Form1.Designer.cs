b484f01418ef3fd42385da91b0d7c87bf1c21b6a
LANA <imanulhaq5@gmail.com>
2022-10-17 16:43:24 +0700

    Add project files.

---
+++ P5_1_1214078/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace P5_1_1214078
+{
+    partial class MaulanaProject
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OutputLabel = new System.Windows.Forms.Label();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.HelloButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // OutputLabel
+            // 
+            this.OutputLabel.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.OutputLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.OutputLabel.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OutputLabel.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.OutputLabel.Location = new System.Drawing.Point(41, 51);
+            this.OutputLabel.Name = "OutputLabel";
+            this.OutputLabel.Size = new System.Drawing.Size(264, 53);
+            this.OutputLabel.TabIndex = 1;
+            this.OutputLabel.Text = "....";
+            this.OutputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(177, 119);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(74, 35);
+            this.ExitButton.TabIndex = 4;
+            this.ExitButton.Text = "&Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // HelloButton
+            // 
+            this.HelloButton.Location = new System.Drawing.Point(68, 119);
+            this.HelloButton.Name = "HelloButton";
+            this.HelloButton.Size = new System.Drawing.Size(87, 35);
+            this.HelloButton.TabIndex = 3;
+            this.HelloButton.Text = "&Say Hello";
+            this.HelloButton.UseVisualStyleBackColor = true;
+            this.HelloButton.Click += new System.EventHandler(this.HelloButton_Click);
+            // 
+            // MaulanaProject
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lime;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.HelloButton);
+            this.Controls.Add(this.OutputLabel);
+            this.Name = "MaulanaProject";
+            this.Text = "MaulanaProject";
+            this.Load += new System.EventHandler(this.MaulanaProject_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label OutputLabel;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Button HelloButton;
+    }
+}
+
---
+++ P5_1_1214078/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P5_1_1214078
+{
+    public partial class MaulanaProject : Form
+    {
+        public MaulanaProject()
+        {
+            InitializeComponent();
+        }
+
+        private void MaulanaProject_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void HelloButton_Click(object sender, EventArgs e)
+        {
+            OutputLabel.Text = "Hello World";
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
